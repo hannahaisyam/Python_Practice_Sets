43b2ae8440e477deed3df7f0717ae34b5b07e95b
Hannah Mohamad <124024537+hannahaisyam@users.noreply.github.com>
2023-02-23 11:48:09 +0800

    Update OOP_Set2.py

--- obj_ort_prog/OOP_Set2.py
+++ obj_ort_prog/OOP_Set2.py
@@ -14,11 +14,11 @@ class Car(Vehicle):
     pass
 
 
-#School_bus = Bus("School Volvo", 180, 12)
-#print(School_bus.color, School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
+School_bus = Bus("School Volvo", 180, 12)
+print(School_bus.color, School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
 
-#car = Car("Audi Q5", 240, 18)
-#print(car.color, car.name, "Speed:", car.max_speed, "Mileage:", car.mileage)
+car = Car("Audi Q5", 240, 18)
+print(car.color, car.name, "Speed:", car.max_speed, "Mileage:", car.mileage)
 
 
 #2 Class Inheritance 
